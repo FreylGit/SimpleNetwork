15b302643ec4c31ac8667a07749a3794e30aa6ae
ANDREY <41635263+FreylGit@users.noreply.github.com>
2022-05-17 11:32:32 +0300

    Добавьте файлы проекта.

---
+++ Neuron.cs
@@ -0,0 +1,86 @@
+﻿
+//Тип функции активации
+public enum Activation
+{
+    Threshold = 0,
+    Sigmoid = 1,
+}
+public class Neuron
+{
+    public double LastError { get; set; }
+    public double Smoothing { get; set; } = 0.00001;
+    public double[] weigth { get; set; }
+    private Activation Activation { get; set; }
+
+    public Neuron(Activation activation = Activation.Threshold)
+    {
+        Random random = new Random();
+        weigth = new double[2];
+        weigth[0] = random.NextDouble();
+        weigth[1] = random.NextDouble();
+        Activation = activation;
+    }
+    public double InputData(double[] input)
+    {
+        var sum = 0.0;
+        for (int i = 0; i < weigth.Length; i++)
+        {
+            sum += weigth[i] * input[i];
+        }
+        if (Activation == Activation.Sigmoid)
+        {
+            return Sigmoid(sum);
+        }
+        if (Activation == Activation.Threshold)
+        {
+            return Threshold(sum);
+        }
+        return sum;
+    }
+
+    //Функция обучения неирона
+    public void Learn(double[] input, double expectedResult)
+    {
+        var actualResult = 0.0;
+        for (int i = 0; i < weigth.Length; i++)
+        {
+            actualResult += weigth[i] * input[i];
+        }
+        if(Activation == Activation.Sigmoid)
+        {
+            actualResult = Sigmoid(actualResult);
+        }
+        if(Activation == Activation.Threshold)
+        {
+            actualResult = Threshold(actualResult);
+        }
+        LastError = expectedResult - actualResult;
+
+        if (actualResult == 0.0)
+        {
+            actualResult = 0.00001;
+        }
+        var correction = (LastError / actualResult) * Smoothing;
+        for (int i = 0; i < weigth.Length; i++)
+        {
+            weigth[i] += correction;
+        }
+    }
+    //Сигмоидная функция
+    private double Sigmoid(double x)
+    {
+        return (1 / (1 + Math.Exp(-x)));
+    }
+    //Пороговая функция
+    private double Threshold(double x)
+    {
+        if (x >= 0.5)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿var neuron = new Neuron(Activation.Threshold);
+double[] input1 = new double[] { 1.0, 1.0 };//Первая строчка таблицы
+double[] input2 = new double[] { 1.0, 0.0 };//Вторая строчка таблицы
+double[] input3 = new double[] { 0.0, 1.0 };//Третья строчка таблицы
+double[] input4 = new double[] { 0.0, 0.0 };//Четвертая строчка таблицы
+
+int i = 0;
+do
+{
+    i++;
+    neuron.Learn(input1, 1.0);
+    neuron.Learn(input2, 0.0);
+    neuron.Learn(input3, 1.0);
+    neuron.Learn(input4, 0);
+
+    neuron.Learn(input2, 0.0);
+    neuron.Learn(input3, 1.0);
+    neuron.Learn(input4, 0);
+    neuron.Learn(input1, 1.0);
+
+    neuron.Learn(input3, 1.0);
+    neuron.Learn(input4, 0);
+    neuron.Learn(input1, 1.0);
+    neuron.Learn(input2, 0.0);
+
+    neuron.Learn(input4, 0);
+    neuron.Learn(input1, 1.0);
+    neuron.Learn(input2, 0.0);
+    neuron.Learn(input3, 1.0);
+    //По очереди меняем порядок обучения чтобы было все четко, может не правильно, но работает 
+}
+while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
+
+Console.WriteLine(neuron.weigth[0] + " " + neuron.weigth[1]);
+Console.WriteLine(neuron.InputData(input1));
+Console.WriteLine(neuron.InputData(input2));
+Console.WriteLine(neuron.InputData(input3));
+Console.WriteLine(neuron.InputData(input4));
+
